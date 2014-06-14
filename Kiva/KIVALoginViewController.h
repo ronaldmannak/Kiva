2a8c2facd3f49ab2753e2140084e9f8728be3c47
Ronald Mannak <ronaldmannak@me.com>
2014-06-14 14:57:23 -0700

    Added login vc

---
+++ Kiva/KIVALoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  KIVALoginViewController.h
+//  Kiva
+//
+//  Created by Ronald Mannak on 6/14/14.
+//  Copyright (c) 2014 Ronald Mannak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KIVALoginViewController : UIViewController
+
+@end
